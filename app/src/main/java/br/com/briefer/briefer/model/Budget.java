1576618513d240e92c39b4ee02922edf553771ad
BuildTools <cassiano.jr.2009@hotmail.com>
2019-05-13 16:46:26 -0300

    Added model for User, Briefing and Budget

---
+++ app/src/main/java/br/com/briefer/briefer/model/Budget.java
@@ -0,0 +1,24 @@
+package br.com.briefer.briefer.model;
+
+import java.util.Date;
+
+public class Budget {
+    private Date time_goal;
+    private double cost;
+
+    public Date getTime_goal() {
+        return time_goal;
+    }
+
+    public void setTime_goal(Date time_goal) {
+        this.time_goal = time_goal;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
