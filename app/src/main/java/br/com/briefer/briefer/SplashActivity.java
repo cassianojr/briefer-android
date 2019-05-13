396c05b9af312a8a2d2920c048ad28caf2859142
BuildTools <cassiano.jr.2009@hotmail.com>
2019-05-13 12:15:47 -0300

    Initial Structure of the app screens

--- app/src/main/java/br/com/briefer/briefer/SplashActivity.java
+++ app/src/main/java/br/com/briefer/briefer/SplashActivity.java
@@ -1,13 +1,22 @@
 package br.com.briefer.briefer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Do all the startup verifications before start the activity
+
+        //start activity
+        Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
